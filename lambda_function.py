a29d0e4e4e0cc99548d60ef23d48956b9bfdc4c7
Sivadevsecops <71688381+Sivadevsecops@users.noreply.github.com>
2020-09-24 00:49:42 -0700

    updated with parjivresp

--- lambda_function.py
+++ lambda_function.py
@@ -3,4 +3,6 @@ import json
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     response = ec2.describe_availability_zones()
+    print(response)
     return {"statuscode": 200, "body": json.dumps(response)}
+    

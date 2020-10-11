32b2a0e7b7476d593d827eda62efd29036e6379f
Your Name <you@example.com>
2020-10-11 17:52:18 +0000

    this is a new commit

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,6 @@ ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     response = ec2.describe_availability_zones()
     print(response)
+    print(Dummy)
     return {"statuscode": 200, "body": json.dumps(response)}
     
